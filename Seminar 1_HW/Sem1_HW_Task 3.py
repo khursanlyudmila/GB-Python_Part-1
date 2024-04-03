9cfae65e64d3be75e4c52776d5616c2c17c9097f
Khursan Lyudmila <khursanlyudmila@gmail.com>
2024-04-03 22:29:47 +0300

    Add file Sem1_HW_Task 3.py

--- Seminar 1_HW/Sem1_HW_Task 3.py
+++ Seminar 1_HW/Sem1_HW_Task 3.py
@@ -4,3 +4,24 @@
 Для генерации случайного числа используйте код:
 from random import randintnum = randint(LOWER_LIMIT, UPPER_LIMIT)'''
 
+import random
+'''from random import randintnum'''
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+attempt = 1
+comp_num = random.randint(LOWER_LIMIT, UPPER_LIMIT)
+print("Программа загадала число. У Вас 10 попыток, чтобы его разгадать!")
+while attempt <= 10:
+    user_num = int(input(str(attempt) + '-а попытка: '))
+    if user_num > comp_num:
+        print("Загаданное число меньше.")
+    elif user_num < comp_num:
+        print("Загаданное число больше.")
+    else:
+        print("Число угадано с %d-й попытки" % attempt)
+        break
+    attempt += 1
+else:
+    print("Все попытки исчерпаны. Было загадано число", comp_num)
+
