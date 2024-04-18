246dc3ac45cf9328433f8ec61afe3f7f5470ca85
Khursan Lyudmila <khursanlyudmila@gmail.com>
2024-04-18 09:56:21 +0300

    Add files in  Seminar 4_HW  folder

--- Seminar 4_HW/Sem4_HW_Task 3.py
+++ Seminar 4_HW/Sem4_HW_Task 3.py
@@ -35,23 +35,17 @@ def menu(balance: Decimal, count: int, is_flag: bool):
         return balance, is_flag
     elif choice == '1':
         balance = give_money(balance)
-        #count += 1
         balance_list.append(money_to_give)
-        #if count % 3 == 0:
-            #balance *= (1 + BONUS)
-        #print('Начислены проценты в размере: ', round(balance * BONUS, 2))
     elif choice == '2':
         balance = get_money(balance)
-        count += 1
-        balance_list.append(-1 * money_to_get)
-        #if count % 3 == 0:
-            #balance *= (1 + BONUS)
-        #print('Начислены проценты в размере: ', round(balance * BONUS, 2))   
+        balance_list.append(-1 * money_to_get) 
     elif choice == '3':
         print('Операции по счету: ', ', '.join(map(str,balance_list)))
     else:
         print('Неверная команда')
-    
+    if count % 3 == 0:
+        balance *= (1 + BONUS)
+        print('Начислены проценты в размере: ', round(balance * BONUS, 2))
     print('Баланс счета: ', round(balance,2))
     print('Счетчик операций: ', count)
     return balance, is_flag
@@ -70,13 +64,8 @@ def get_money(balance: Decimal):
             procent = MAX_COMISSION
 
         if money_to_get + procent <= balance:
-            return balance - (money_to_get + procent)
-        if money_to_get + procent <= balance == True:
             count +=1
-            if count % 3 == 0:
-                balance *= (1 + BONUS)
-            print('Начислены проценты в размере: ', round(balance * BONUS, 2))
-            return balance
+            return balance - (money_to_get + procent)
         else:
             print('Недостаточно средств для снятия!')
             return balance
@@ -91,13 +80,8 @@ def give_money(balance: Decimal):
     global count
     money_to_give = Decimal(input('Введите сумму пополнения: '))
     if money_to_give % MIN_SUM == 0:
-        return balance + money_to_give
-    if money_to_give % MIN_SUM == 0 == True:
         count +=1
-        if count % 3 == 0:
-            balance *= (1 + BONUS)
-        print('Начислены проценты в размере: ', round(balance * BONUS, 2))
-        return balance
+        return balance + money_to_give
     else:
         print('Недостаточно средств для пополнения, сумма не кратна 50!\n')
         return balance
@@ -106,95 +90,9 @@ def give_money(balance: Decimal):
 if __name__ == '__main__':
     print('Добро пожаловать в программу банкомат!\n')
     balance = 0
-    count = 0
+    count = 1
     is_flag = True
     balance_list = []
     while is_flag:
         balance, is_flag = menu(balance, count, is_flag)
         
-'''
-from datetime import date
-
-bank = 0
-count = 0
-percent_take = 0.015
-percent_add = 0.03
-percent_tax = 0.01
-
-def add_bank(cash: float) -> None:
-    global bank
-    global count
-    bank += cash
-    count += 1
-    if count % 3 == 0:
-        bank = bank + percent_add * bank
-        print("начислены проценты в размере: ", percent_add * bank)
-
-def take_bank(cash: float) -> None:
-    global bank
-    global count
-    bank -= cash
-    count += 1
-
-    if cash * percent_take < 30:
-        bank -= 30
-        print("списаны проценты за cash: ", 30)
-    elif cash * percent_take > 600:
-        bank -= 600
-        print("списаны проценты за cash: ", 600)
-    else:
-        bank -= cash * percent_take
-        print("списаны проценты за cash: ", cash * percent_take)
-    if count % 3 == 0:
-        bank = bank + percent_add * bank
-        print("начислены проценты в размере: ", percent_add * bank)
-
-
-def exit_bank():
-    print("Рады вас видетеь снова!\n")
-    exit()
-
-def check_bank() -> int:
-    while True:
-        cash = int(input("Введите сумму опреации кратно 50\n"))
-        if cash % 50 == 0:
-            return cash
-
-list_operation = []
-
-while True:
-    action = input("1 - снять деньги\n2 - пополнить\n3 - баланс\n4 - вывести историю операций\n5 - выйти\n")
-
-    if action == '1':
-        if bank > 5_000_000:
-            bank = bank - bank * percent_tax
-            print("списан налог на богатство: ", bank * percent_tax)
-        cash = check_bank()
-        if cash < bank:
-            take_bank(cash)
-
-            list_operation.append([str(date.today()), -1 * cash])
-        else:
-            print("no money\n")
-        if bank > 5_000_000:
-            bank = bank - bank * percent_tax
-            print("списан налог на богатство: ", bank * percent_tax)
-        print("Баланс = ", bank)
-    elif action == '2':
-        cash = check_bank()
-        add_bank(cash)
-        if bank > 5_000_000:
-            bank = bank - bank * percent_tax
-            print("списан налог на богатство: ", bank * percent_tax)
-        print("Баланс = ", bank)
-
-        list_operation.append([str(date.today()), cash])
-
-    elif action == '3':
-        print("Баланс = ", bank)
-    elif action == '4':
-        print(list_operation)
-    else:
-        exit_bank()
-
-        '''
---
+++ Seminar 4_HW/Sem4_HW_Task 3ex.py
@@ -0,0 +1,87 @@
+"""
+Пример решения задачи с банкоматом!
+"""
+
+from datetime import date
+
+bank = 0
+count = 0
+percent_take = 0.015
+percent_add = 0.03
+percent_tax = 0.01
+
+def add_bank(cash: float) -> None:
+    global bank
+    global count
+    bank += cash
+    count += 1
+    if count % 3 == 0:
+        bank = bank + percent_add * bank
+        print("начислены проценты в размере: ", percent_add * bank)
+
+def take_bank(cash: float) -> None:
+    global bank
+    global count
+    bank -= cash
+    count += 1
+
+    if cash * percent_take < 30:
+        bank -= 30
+        print("списаны проценты за cash: ", 30)
+    elif cash * percent_take > 600:
+        bank -= 600
+        print("списаны проценты за cash: ", 600)
+    else:
+        bank -= cash * percent_take
+        print("списаны проценты за cash: ", cash * percent_take)
+    if count % 3 == 0:
+        bank = bank + percent_add * bank
+        print("начислены проценты в размере: ", percent_add * bank)
+
+
+def exit_bank():
+    print("Рады вас видетеь снова!\n")
+    exit()
+
+def check_bank() -> int:
+    while True:
+        cash = int(input("Введите сумму опреации кратно 50\n"))
+        if cash % 50 == 0:
+            return cash
+
+list_operation = []
+
+while True:
+    action = input("1 - снять деньги\n2 - пополнить\n3 - баланс\n4 - вывести историю операций\n5 - выйти\n")
+
+    if action == '1':
+        if bank > 5_000_000:
+            bank = bank - bank * percent_tax
+            print("списан налог на богатство: ", bank * percent_tax)
+        cash = check_bank()
+        if cash < bank:
+            take_bank(cash)
+
+            list_operation.append([str(date.today()), -1 * cash])
+        else:
+            print("no money\n")
+        if bank > 5_000_000:
+            bank = bank - bank * percent_tax
+            print("списан налог на богатство: ", bank * percent_tax)
+        print("Баланс = ", bank)
+    elif action == '2':
+        cash = check_bank()
+        add_bank(cash)
+        if bank > 5_000_000:
+            bank = bank - bank * percent_tax
+            print("списан налог на богатство: ", bank * percent_tax)
+        print("Баланс = ", bank)
+
+        list_operation.append([str(date.today()), cash])
+
+    elif action == '3':
+        print("Баланс = ", bank)
+    elif action == '4':
+        print(list_operation)
+    else:
+        exit_bank()
