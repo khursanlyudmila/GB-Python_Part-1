003c325b8a4e1e06b64101860082c046f7231814
Khursan Lyudmila <khursanlyudmila@gmail.com>
2024-04-18 20:10:42 +0300

    Modify Sem5_Task 7.py file

--- Seminar 5/Sem5_Task 7.py
+++ Seminar 5/Sem5_Task 7.py
@@ -18,7 +18,7 @@ def prime(n):
         return True
     if n % 2 == 0:
         return False
-    for i in range(3, int(n ** 0.5) + 1):
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
@@ -33,3 +33,26 @@ def generate_simple():
 j = generate_simple()
 for _ in range(int(input("введите число N: "))):
     print(next(j), end=' ')
+
+""" Цикл переписан до какого-либо числа...
+def prime(n):
+    if n<3:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def generate_simple(m):
+    for i in range(2,m):
+        if i > 200:
+            raise StopIteration ()
+        if prime(i):
+            yield i
+
+j = generate_simple(int(input("введите число N: ")))
+for i in j:
+    print(i, end=" ")
+"""
